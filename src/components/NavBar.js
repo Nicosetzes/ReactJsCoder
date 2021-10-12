f553739a4b91f5aca0fdf79044745ea2774e6260
Nicolás Setzes <nicosetzes@gmail.com>
2021-10-12 11:02:37 -0300

    first component of the project: the NavBar.

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+export const NavBar = () => {
+    return (
+        <nav>
+            <div className="logo">Logo</div>
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Link 1</a>
+                </li>
+                <li>
+                    <a href="#">Link 2</a>
+                </li>
+                <li>
+                    <a href="#">Link 3</a>
+                </li>
+                <li>
+                    <a href="#">Link 4</a>
+                </li>
+            </ul>
+            <div className="login">
+                <button>Login</button>
+            </div>
+        </nav>
+    );
+}
