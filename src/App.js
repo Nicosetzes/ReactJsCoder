6dc6cad9d5ba08c282ea8b95f71118e87646e909
Nicolás Setzes <nicosetzes@gmail.com>
2021-10-30 15:48:09 -0300

    routing.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import { NavBar } from "./components/NavBar";
 import "./components/NavBar.css";
 import { ItemListContainer } from "./components/ItemListContainer";
 import "./components/ItemListContainer.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar />
-      </header>
-      <main>
-        <ItemListContainer greeting="JSX" />
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <NavBar />
+        </header>
+        <Switch>
+          <Route exact path="/">
+            <main>
+              <ItemListContainer greeting="JSX" />
+            </main>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
